c8c45be083f2f644a0fa16d4046c492cefcb2199
nicoleorzan <orzan.nicole@gmail.com>
2018-02-24 07:47:23 +0100

    saving data for logaritmic test

--- c++/test/scrpit.sh
+++ c++/test/scrpit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for size in 500000 600000; do  #300000 400000 500000 600000 700000 800000 900000 1000000; do
+for size in 100000 200000 300000 400000 500000 600000 700000 800000 900000 1000000 1200000; do
     echo ${size}
     ./testlog.x ${size} >output_${size}.txt
     grep -E "before" output_${size}.txt | awk '{print $4}'>>output_before_${size}
