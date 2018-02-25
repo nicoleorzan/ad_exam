a47ad2d043d471a1b8683981ab439852b0b9a5c4
nicoleorzan <orzan.nicole@gmail.com>
2018-02-25 09:30:45 +0100

    adding changes in my repo

--- c++/test/scrpit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for size in 100000 200000 300000 400000 500000 600000 700000 800000 900000 1000000 1200000; do
-    echo ${size}
-    ./testlog.x ${size} >output_${size}.txt
-    grep -E "before" output_${size}.txt | awk '{print $4}'>>output_before_${size}
-    grep -E "after" output_${size}.txt | awk '{print $4}'>>output_after_${size}
-done
